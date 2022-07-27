b5e1f12679357e58702923a230c59fcb033def4f
Sergey Havrik <s.gavrik83@gmail.com>
2022-07-27 15:31:46 +0300

    created repository

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react'
+import css from 'components/App.module.css'
+
+
+export class App extends Component {
+  
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0
+  // }
+
+  // handleButtonGood = () => {
+  //   this.setState(prevState => ({
+  //     good: prevState.good + 1,
+  //   }))
+  // };
+
+
+  render() {
+    return (      
+      <div className={css.container}>
+        <p>Test</p>
+      </div>
+    );
+  }
+}
+
+export default App;
